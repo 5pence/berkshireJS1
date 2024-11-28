00f865cac6abbdb06491816a767c4ae8995866bd
spencer barriball <sbarriball@me.com>
2024-11-28 15:51:53 +0000

    add difference varibale names to make clearer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 // getters
-const question = document.getElementById('question');
-const answer0 = document.getElementById('answer0');
-const answer1 = document.getElementById('answer1');
-const answer2 = document.getElementById('answer2');
-const answer3 = document.getElementById('answer3');
-const score = document.getElementById('score');
+const questionVar = document.getElementById('question');
+const answer0Var = document.getElementById('answer0');
+const answer1Var = document.getElementById('answer1');
+const answer2Var = document.getElementById('answer2');
+const answer3Var = document.getElementById('answer3');
+const scoreVar = document.getElementById('score');
 
 
 let questionNumber = 0;
@@ -28,27 +28,27 @@ const questions = [
     },
 ]
 
-
 // load question
 function loadQuestion(questionNumber) {
-    question.innerText = questions[questionNumber].question;
+    questionVar.innerText = questions[questionNumber].question;
 }
 
 // load answers
 function loadAnswers(questionNumber) {
-    answer0.innerText = questions[questionNumber].answers[0];
-    answer1.innerText = questions[questionNumber].answers[1];
-    answer2.innerText = questions[questionNumber].answers[2];
-    answer3.innerText = questions[questionNumber].answers[3];
+    answer0Var.innerText = questions[questionNumber].answers[0];
+    answer1Var.innerText = questions[questionNumber].answers[1];
+    answer2Var.innerText = questions[questionNumber].answers[2];
+    answer3Var.innerText = questions[questionNumber].answers[3];
 }
 
 // wait for them to press an answer (onclick)
 function checkAnswer(answerSelected) {
+    console.log('im here! entyered: ', answerSelected);
     // check if they got answer correct
     if (answerSelected === questions[questionNumber].correct) {
         // if they did +1 score and update screen
         currentScore++;
-        score.innerText = currentScore;
+        scoreVar.innerText = currentScore;
     }
     // see if end of quiz is not reached
     if (questionNumber < questions.length-1) {
